008d0ee6ee14a99f09529dcf0a98893af3fb75e4
KubaTaba1uga <luna7337anul@proton.me>
2025-03-10 18:41:10 +0100

    inline overlay

--- README.md
+++ README.md
@@ -2,28 +2,21 @@
 
 ## Prepare dts file
 
-We need to compile device tree overlay to configure GPIO pin and use it from our kernel module. To do so follow instructions below.
-
-Compile dtb file:
+Add to arch/arm64/boot/dts/broadcom/bcm2712-rpi-5-b.dts after `leds: leds` section
 ```
-make dt
-```
-
-And copy it on boot partition into overlays dir:
-```
-$ sudo cp simple_sensor_cache.dtbo /opt/tftp/b7afe72d/overlays/
+    simple_sensor_cache_device {
+        compatible = "raspberrypi,simple_sensor_cache_device";
+        data-gpios = <&rp1_gpio 15 0>;
+        status = "okay";
+    };
 ```
 
-Now add to config.txt:
-```
-dtoverlay=simple_sensor_cache
-```
-
-Reboot and confirm overlay is working:
+Recompile dtses and upload to your boot partition. Once they are there boot up rpi5 and confirm that our custom gpio is recognized as device:
 ```
 # find /proc/device-tree/ | grep simple
-/proc/device-tree/axi/pcie@120000/rp1/gpio@d0000/simple_sensor_cache_device
-/proc/device-tree/axi/pcie@120000/rp1/gpio@d0000/simple_sensor_cache_device/data-gpios
-/proc/device-tree/axi/pcie@120000/rp1/gpio@d0000/simple_sensor_cache_device/compatible
-/proc/device-tree/axi/pcie@120000/rp1/gpio@d0000/simple_sensor_cache_device/name
+/proc/device-tree/simple_sensor_cache_device
+/proc/device-tree/simple_sensor_cache_device/data-gpios
+/proc/device-tree/simple_sensor_cache_device/compatible
+/proc/device-tree/simple_sensor_cache_device/status
+/proc/device-tree/simple_sensor_cache_device/nam
 ```
--- simple_sensor_cache.c
+++ simple_sensor_cache.c
@@ -7,8 +7,6 @@
  */
 #include <linux/delay.h>
 #include <linux/err.h>
-#include <linux/gpio.h>          // For gpio_to_desc()
-#include <linux/gpio/consumer.h> // For descriptor-based GPIO
 #include <linux/gpio/consumer.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -16,34 +14,48 @@
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
 
-struct simple_sensor_cache {
+struct simple_sensor_cache_data {
   struct gpio_desc *gpio;
 };
 
 static int simple_sensor_cache_probe(struct platform_device *pdev) {
-  struct simple_sensor_cache *ow;
+  static struct simple_sensor_cache_data *data;
 
-  ow = devm_kzalloc(&pdev->dev, sizeof(*ow), GFP_KERNEL);
-  if (!ow)
+  // Memory allocated with this function is automatically freed on driver
+  //   detach.
+  data = devm_kzalloc(&pdev->dev, sizeof(struct simple_sensor_cache_data),
+                      GFP_KERNEL);
+  if (!data)
     return -ENOMEM;
 
-  ow->gpio = devm_gpiod_get(&pdev->dev, "data", GPIOD_OUT_HIGH);
-  if (IS_ERR(ow->gpio))
-    return dev_err_probe(&pdev->dev, PTR_ERR(ow->gpio), "Failed to get GPIO\n");
+  // Resources allocated by this function are automatically freed on driver
+  //   detach.
+  data->gpio = devm_gpiod_get(
+      &pdev->dev,
+      // This value needs to match xxx-gpios name in device in dts file.
+      "data", GPIOD_IN);
+  if (IS_ERR(data->gpio)) {
+    return dev_err_probe(&pdev->dev, PTR_ERR(data->gpio),
+                         "Failed to get GPIO\n");
+  }
 
-  platform_set_drvdata(pdev, ow);
+  // This functions sets device data so it can be acessed from anwyehre in this
+  //   device ctx.
+  platform_set_drvdata(pdev, data);
+
   dev_info(&pdev->dev, "Custom one-wire GPIO driver probed\n");
 
   return 0;
 }
 
-static int simple_sensor_cache_remove(struct platform_device *pdev) {
+static void simple_sensor_cache_remove(struct platform_device *pdev) {
   dev_info(&pdev->dev, "Custom one-wire GPIO driver removed\n");
-  return 0;
 }
 
 static const struct of_device_id simple_sensor_cache_of_match[] = {
     {
+        // This is a unique value which should match `compatibile` field in
+        // overlay.
         .compatible = "raspberrypi,simple_sensor_cache_device",
     },
     {},
@@ -52,7 +64,7 @@ MODULE_DEVICE_TABLE(of, simple_sensor_cache_of_match);
 
 static struct platform_driver simple_sensor_cache_driver = {
     .probe = simple_sensor_cache_probe,
-    .remove = simple_sensor_cache_remove,
+    .remove_new = simple_sensor_cache_remove,
     .driver =
         {
             .name = "simple_sensor_cache_gpio",
