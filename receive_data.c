ae8c9b73b66c8b5c70a3ac2d849c5ba18d6c6d25
KubaTaba1uga <luna7337anul@proton.me>
2025-03-11 12:45:10 +0100

    improve solution

--- receive_data.c
+++ receive_data.c
@@ -20,26 +20,32 @@ int simple_sensor_cache_receive_data(struct simple_sensor_cache_data *data) {
       return 1;
     }
 
-    // We need to wait untill bit transmission starts
-    udelay(50 - (i == 0 ? 10 : 0));
+    // We need to wait for the voltage to be pulled up
+    int waits = 0;
+    while (!gpiod_get_value(data->gpio) && ++waits < 100) {
+      udelay(1);
+    }
 
-    // High voltage indicate bit transmission start
-    if (gpiod_get_value(data->gpio) != 1) {
-      LKM_PRINT_ERR(data->pdev, "Sensor is not pulling up voltage: i=%i\n", i);
+    if (waits >= 100) {
+      LKM_PRINT_ERR(data->pdev,
+                    "Sensor is not pulling up voltage: waits=%i, i=%i\n", waits,
+                    i);
       return 1;
     }
 
     // Now we need to receive one bit, if voltage was high for more than 70us it
     // is 1 if shorter it is 0
-    int waits = 0;
-    while (gpiod_get_value(data->gpio) && ++waits < 10) {
+    waits = 0;
+    while (gpiod_get_value(data->gpio) && ++waits < 100) {
       udelay(10);
     }
 
-    if (waits >= 6) {
-      result[i] = 1;
-    } else {
+    if (2 <= waits && waits <= 4) { // If between 20us and 40us it's 0
       result[i] = 0;
+    } else if (6 <= waits && waits <= 8) { // If between 60us and 80us it's 1
+      result[i] = 1;
+    } else { // If longer than 80us it's unrecognized value
+      result[i] = -1;
     }
   }
 
